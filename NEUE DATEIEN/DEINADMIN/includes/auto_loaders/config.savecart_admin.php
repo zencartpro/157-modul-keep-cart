fea58932c0bee4c11ba87674d9a08c5695970733
Jörg Schönberger <office@webchills.at>
2023-06-27 09:00:38 +0200

    Version 2.0.4

--- NEUE DATEIEN/DEINADMIN/includes/auto_loaders/config.savecart_admin.php
+++ NEUE DATEIEN/DEINADMIN/includes/auto_loaders/config.savecart_admin.php
@@ -3,13 +3,13 @@
  * Keep Cart Plugin
  * Copyright C.J.Pinder 2009 (http://www.zen-unlocked.com)
  *
- * @copyright Copyright 2003-2022 Zen Cart Development Team
+ * @copyright Copyright 2003-2023 Zen Cart Development Team
  * Zen Cart German Version - www.zen-cart-pro.at
  * @copyright Portions Copyright 2003 osCommerce
  * @license https://www.zen-cart-pro.at/license/3_0.txt GNU General Public License V3.0
- * @version $Id: config.savecart_admin.php 2022-07-27 10:00:40Z webchills $
+ * @version $Id: config.savecart_admin.php 2023-06-27 08:00:40Z webchills $
  */
-$autoLoadConfig[200][] = array(
+$autoLoadConfig[200][] = [
     'autoType' => 'init_script',
     'loadFile' => 'init_savecart_admin.php'
-);
+];
